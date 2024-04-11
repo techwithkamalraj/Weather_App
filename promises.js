0156ac52f8eaadbf0ee6231ea9cffe67fc786364
Kamalraj David Chitturi <99992423+techwithkamalraj@users.noreply.github.com>
2024-04-11 19:09:36 +0530

    "Added https"

--- promises.js
+++ promises.js
@@ -1,43 +1,38 @@
-
-
-
-
-function getWeatherData(){
+function getWeatherData() {
     let location_name = document.getElementById('city_name').value;
     const api_key = "630d0091c02fdec693cd3d6dd0f47955";
-    const apiUrl = `http://api.weatherstack.com/current?access_key=${api_key}&query=${location_name}`;
-    
-    const weather_Data = new Promise((res,rej)=>{
-        
+    const apiUrl = `https://api.weatherstack.com/current?access_key=${api_key}&query=${location_name}`;
+
+    const weather_Data = new Promise((res, rej) => {
         fetch(apiUrl)
-        .then((response)=>{
-            if(!response.ok){
-                throw new Error("Errow while fetching api");
-            }
-            else{
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error("Error while fetching API");
+                }
                 return response.json();
-            }
-        }).then((data)=>{
-            res(data);
-        })
-        .catch((error)=>{
-            rej(error);
-        })
-        
+            })
+            .then((data) => {
+                res(data);
+            })
+            .catch((error) => {
+                rej(error);
+            });
+    });
+
+    weather_Data.then((real_data) => {
+        console.log(real_data);
+
+       
+
     })
-    
-    weather_Data.then((real_data)=>{
-        // console.log(real_data);
-        
-        document.getElementById('City_Name').textContent = `${real_data.location.name}`
-        document.getElementById('date').textContent = `${real_data.location.localtime}`
-        document.getElementById('temprature').textContent = `${real_data.current.temperature} ℃`
-        
-    })
-    .catch((err)=>{
+    .catch((err) => {
         console.log(err);
-    })
-    
+    });
 }
 
 getWeatherData();
+
+
+ // document.getElementById('city_Name').textContent = `${real_data.location.name}`;
+        // document.getElementById('date').textContent = `${real_data.location.localtime}`;
+        // document.getElementById('temperature').textContent = `${real_data.current.temperature} ℃`;
